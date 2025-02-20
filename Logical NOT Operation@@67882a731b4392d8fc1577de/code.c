c53c19e26ff1a8d14965568a304e342c8df06b86
Nimar-Sharma <nimarsharma7@gmail.com>
2025-02-20 19:33:03 +0530

    Solution to Logical NOT Operation

--- Logical NOT Operation@@67882a731b4392d8fc1577de/code.c
+++ Logical NOT Operation@@67882a731b4392d8fc1577de/code.c
@@ -1,14 +1,12 @@
-// Find the Sum of the Digits of a Given Number
 #include <stdio.h>
 int main(){
-    int x, sum =0;
-    printf("qwqqw");
-    scanf("%d", &x);
-    while(x != 0){
-        x%=10;
-        int digit = x;
-        sum+=x;
-        printf("%d" , sum);
+    int x;
+    scanf("%d", &f);
+    if(x !< 0){
+        printf("True");
+    }
+    else{
+        printf("False");
     }
     return 0;
 }
